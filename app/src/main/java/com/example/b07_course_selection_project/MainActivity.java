3621a657e61bf6a27856bc862d0d442d63e7a158
HyphenHook <fghjru1@gmail.com>
2022-11-16 13:15:18 -0500

    Initialized binding and FirebaseAuth

--- app/src/main/java/com/example/b07_course_selection_project/MainActivity.java
+++ app/src/main/java/com/example/b07_course_selection_project/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.b07_course_selection_project.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
