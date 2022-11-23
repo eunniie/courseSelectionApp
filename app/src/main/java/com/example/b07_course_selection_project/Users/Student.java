cb571c2096c25fe3a2d644b19cec214d8fb17094
HyphenHook <fghjru1@gmail.com>
2022-11-23 16:36:15 -0500

    Refactored according to MVP. Also refactored code to use binding instead of findId

--- app/src/main/java/com/example/b07_course_selection_project/Users/Student.java
+++ app/src/main/java/com/example/b07_course_selection_project/Users/Student.java
@@ -1,4 +1,20 @@
 package com.example.b07_course_selection_project.Users;
 
+import com.example.b07_course_selection_project.Course.Course;
+
+import java.util.ArrayList;
+
 public class Student {
+    protected String email;
+    protected ArrayList<String> completedCoursesCode = new ArrayList<>();
+
+    public void addCoursesCode(Course course1){
+        this.completedCoursesCode.add(course1.getCourseCode());
+    }
+
+    public String[] getCoursesCode(){
+        String[] arr = new String[completedCoursesCode.size()];
+        return completedCoursesCode.toArray(arr);
+    }
+
 }
--- app/src/main/java/com/example/b07_course_selection_project/completedCourses.java
+++ app/src/main/java/com/example/b07_course_selection_project/completedCourses.java
@@ -5,18 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
+import com.example.b07_course_selection_project.Users.Student;
 import com.example.b07_course_selection_project.databinding.ActivityCompletedCoursesBinding;
 import com.example.b07_course_selection_project.databinding.ActivityMainBinding;
 
 public class completedCourses extends AppCompatActivity {
     private ActivityCompletedCoursesBinding binding;
+    ListView coursesList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCompletedCoursesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //Set courses codes to ListView
+        coursesList = (ListView) binding.completedCourses;
+        String[] arr = {"courses"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        coursesList.setAdapter(arrayAdapter);
 
         //Make button open activity
         binding.backButton.setOnClickListener(new View.OnClickListener() {
