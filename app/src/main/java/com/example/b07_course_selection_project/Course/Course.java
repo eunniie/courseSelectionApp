075149c788634f858157738b41a1cc5b5d93ee85
MirrorLimit <113953454+MirrorLimit@users.noreply.github.com>
2022-11-23 12:53:38 -0500

    Added the course object and methods to manipulate it. Don't yet know the time we're using for the schedule so it might need to be changed later.

---
+++ app/src/main/java/com/example/b07_course_selection_project/Course/Course.java
@@ -0,0 +1,85 @@
+package com.example.b07_course_selection_project.Course;
+
+import java.util.ArrayList;
+
+//TODO: change changeTimeOffered appropriately based on the system of time we are using!
+
+public class Course {
+    private String name;
+    private String code;
+    private ArrayList<String> preReq = new ArrayList<>();
+    private String timeOffered;
+
+    public Course(String name, String code, ArrayList<String> preReq, String timeOffered) {
+        this.name = name;
+        this.code = code;
+        this.preReq = preReq;
+        this.timeOffered = timeOffered;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCourseCode() {
+        return this.code;
+    }
+
+    public Course [] getPreReq() {
+        Course [] arr = new Course[preReq.size()];
+        arr = preReq.toArray(arr); //making sure the array has the size and is of the same type
+
+        return arr;
+    }
+
+    public String getTimeOffered() {
+        return this.timeOffered;
+    }
+
+    public boolean changeName (String input) {
+        if (input != null)
+        {
+            this.name = input;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean changeCourseCode (String input) {
+        if (input != null)
+        {
+            this.code = input;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteOnePreReq (Course input) {
+        int index = this.preReq.indexOf(input);
+
+        if (index != -1 && this.preReq.size() != 0)
+        {
+            this.preReq.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean changeTimeOffered (String input) {
+        if (input.equalsIgnoreCase("winter")) {
+            this.timeOffered = input;
+            return true;
+        }
+
+        else if (input.equalsIgnoreCase("summer")) {
+            this.timeOffered = input;
+            return true;
+        }
+
+        else if (input.equalsIgnoreCase("fall")) {
+            this.timeOffered = input;
+            return true;
+        }
+        return false;
+    }
+}
