64a7fd571773a1950577c258135658e4a845f209
Vicky <yangvicky8787@gmail.com>
2022-11-24 17:53:43 -0500

    Added dialog for students to add courses they want to take

--- app/src/main/java/com/example/b07_course_selection_project/Student_Panel.java
+++ app/src/main/java/com/example/b07_course_selection_project/Student_Panel.java
@@ -1,18 +1,24 @@
 package com.example.b07_course_selection_project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.b07_course_selection_project.databinding.ActivityStudentPanelBinding;
 
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Student_Panel extends AppCompatActivity {
     private ActivityStudentPanelBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +32,42 @@ public class Student_Panel extends AppCompatActivity {
                 startActivity(new Intent(Student_Panel.this, completedCourses.class));
             }
         });
-        binding.timeline.setOnClickListener(new View.OnClickListener() {
+        //make button display dialog
+        binding.timeline.setOnClickListener(view -> displayAddCourses());
+    }
+
+    //To get the courses that the student wants to take in the future:
+    //TODO: Get courses from database
+    private String[] courses = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}; //The courses to display in dialog
+    private boolean[] checkedCourses = new boolean[courses.length]; //Stores the checks
+    private List<String> selectedCourses = new ArrayList<>(); //The courses student selects
+
+    //Displays the list of courses for user to pick
+    private void displayAddCourses() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Select the course(s) you would like to take:");
+
+        //Stores courses selected
+        dialogBuilder.setMultiChoiceItems(courses, checkedCourses,
+                (dialogInterface, course, isSelected) -> {
+                    if (isSelected) {
+                        selectedCourses.add(courses[course]);
+                    } else {
+                        selectedCourses.remove(courses[course]);
+                    }
+                }
+        );
+
+        //Buttons go to timeline activity or cancel
+        dialogBuilder.setPositiveButton("Generate", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Student_Panel.this, Timeline.class));
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent in = new Intent(Student_Panel.this, Timeline.class);
+                startActivity(in);
             }
         });
+        dialogBuilder.setNeutralButton("Cancel", (dialog, cancel) -> dialog.dismiss());
+
+        dialogBuilder.show();
     }
 }
