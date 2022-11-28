f88e384b512cc479c5c9c1b4bbd517bd76ffa000
Vicky <yangvicky8787@gmail.com>
2022-11-28 01:36:51 -0500

    Implementing button features in timeline

--- app/src/main/java/com/example/b07_course_selection_project/Timeline.java
+++ app/src/main/java/com/example/b07_course_selection_project/Timeline.java
@@ -3,6 +3,8 @@ package com.example.b07_course_selection_project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +14,6 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.b07_course_selection_project.Course.Course;
-import com.example.b07_course_selection_project.databinding.ActivityStudentPanelBinding;
 import com.example.b07_course_selection_project.databinding.ActivityTimelineBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,38 +26,58 @@ import java.util.List;
 
 public class Timeline extends AppCompatActivity {
     private ActivityTimelineBinding binding;
+    private List<String> selected = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTimelineBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        ArrayList<String> selected = getSelection();
+        getSelection();
 
         //Make button open activity
         binding.back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(Timeline.this, Student_Panel.class));
+                startActivity(new Intent(Timeline.this, Student_Panel.class));
+            }
+        });
+        binding.restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSelection();
             }
         });
     }
 
-    private ArrayList<String> getSelection(){
-        ArrayList<String> courses = new ArrayList<>();
-        ArrayList<String> selectedCourses = new ArrayList<>();
+    private void getSelection(){
+        selected.clear();
+
+        List<String> courses = new ArrayList<>();
         ArrayAdapter<String> arrayAdapter = new
                 ArrayAdapter<String>(Timeline.this, android.R.layout.simple_list_item_multiple_choice,
                 courses);
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Courses");
+       DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Courses");
+
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                courses.clear();
                 for (DataSnapshot snap: snapshot.getChildren()){
                     Course c = snap.getValue(Course.class);
                     courses.add(c.getCode());
+                }
+                if (courses.isEmpty()){
+                    new AlertDialog.Builder(Timeline.this).setMessage
+                                    ("There are no courses that you have not already taken, please contact an Admin.")
+                            .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    startActivity(new Intent(Timeline.this, Student_Panel.class));
+                                }
+                            }).show();
+                    return;
                 }
                 arrayAdapter.notifyDataSetChanged();
             }
@@ -65,21 +86,35 @@ public class Timeline extends AppCompatActivity {
 
             }
         });
+
         binding.timeline.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         binding.timeline.setAdapter(arrayAdapter);
         binding.timeline.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(selectedCourses.contains((String) adapterView.getItemAtPosition(i))){
-                    selectedCourses.remove((String) adapterView.getItemAtPosition(i));
-                    Toast.makeText(Timeline.this, "Deleted:" +(String) adapterView.getItemAtPosition(i), Toast.LENGTH_SHORT).show();
+                if(selected.contains((String) adapterView.getItemAtPosition(i))){
+                    selected.remove((String) adapterView.getItemAtPosition(i));
+                    stop(courses, arrayAdapter);
                 }else{
-                    selectedCourses.add((String) adapterView.getItemAtPosition(i));
-                    Toast.makeText(Timeline.this, "Added:" +(String) adapterView.getItemAtPosition(i), Toast.LENGTH_SHORT).show();
+                    selected.add((String) adapterView.getItemAtPosition(i));
+                    stop(courses, arrayAdapter);
                 }
 
             }
         });
-        return selectedCourses;
+    }
+
+    private void stop(List<String> courses, ArrayAdapter<String> arrayAdapter){
+        binding.gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                courses.clear();
+                arrayAdapter.notifyDataSetChanged();
+                for (String s :selected){
+                    Toast.makeText(Timeline.this,s, Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        });
     }
 }
