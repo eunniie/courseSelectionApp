d508fc533ab64028dde06645b339e1c154e66713
GGCav <jeffreyhe406@gmail.com>
2022-11-29 16:41:12 -0500

    Refactored according to MVP. Also refactored code to use binding instead of findId

--- app/src/main/java/com/example/b07_course_selection_project/completedCourses.java
+++ app/src/main/java/com/example/b07_course_selection_project/completedCourses.java
@@ -11,16 +11,20 @@ import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import com.example.b07_course_selection_project.Course.Course;
 import com.example.b07_course_selection_project.Users.Student;
 import com.example.b07_course_selection_project.databinding.ActivityCompletedCoursesBinding;
 import com.example.b07_course_selection_project.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,44 +33,40 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-//TODO: implement getCourses using the same way as displaying the course
 
 public class completedCourses extends AppCompatActivity {
     private ActivityCompletedCoursesBinding binding;
+    private FirebaseAuth mAuth;
     private List<String> completedCourses = new ArrayList<>();
     private List<Course> courses =new ArrayList<>();
+    private List<String> code = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCompletedCoursesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
         binding.searchCourse.setVisibility(View.GONE);
 
-        getCompletedCourses();
-
-        binding.backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-    }
-
-    //Set completed courses to ListView
-    private void getCompletedCourses(){
+        //Set courses codes to ListView
         ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, completedCourses);
+        arrayAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,code);
 
         binding.courseList.setAdapter(arrayAdapter1);
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users")
-                .child("Students").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("completedCoursesCode");
-        ref.addValueEventListener(new ValueEventListener() {
+        FirebaseDatabase.getInstance().getReference().child("Users")
+                .child("Students").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("completedCoursesCode").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot snap: snapshot.getChildren()){
-                    completedCourses.add(snap.getValue(String.class));
+                if (binding.searchCourse.getVisibility() == View.GONE) {
+                    for (DataSnapshot snap : snapshot.getChildren()) {
+                        completedCourses.add(snap.getValue(String.class));
+                        arrayAdapter1.notifyDataSetChanged();
+                    }
+                    Collections.sort(completedCourses);
                     arrayAdapter1.notifyDataSetChanged();
                 }
             }
@@ -76,19 +76,106 @@ public class completedCourses extends AppCompatActivity {
 
             }
         });
-    }
+        Log.d("COMPLETED COURSES: ", completedCourses.toString());
+        binding.courseList.setAdapter((arrayAdapter1));
 
-    private void getCourses() {
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Courses");
-        ref.addValueEventListener(new ValueEventListener() {
+        //Make button open activity
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot snap : snapshot.getChildren()) {
-                    courses.add(snap.getValue(Course.class));
+            public void onClick(View v) {
+                if (binding.searchCourse.getVisibility() == View.GONE) {
+                    startActivity(new Intent(completedCourses.this, Student_Panel.class));
+                }
+                else{
+                    binding.courseList.setAdapter(arrayAdapter1);
+                    binding.searchCourse.setVisibility(View.GONE);
                 }
             }
+        });
+
+        //Set up the add button and adding course panel
+        binding.addCourses.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onClick(View v){
+                binding.searchCourse.setVisibility(View.VISIBLE);
+
+
+                FirebaseDatabase.getInstance().getReference().child("Courses").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot snap : snapshot.getChildren()) {
+                            courses.add(snap.getValue(Course.class));
+                            code.add(snap.getValue(Course.class).getCode());
+                            arrayAdapter2.notifyDataSetChanged();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+                binding.courseList.setAdapter(arrayAdapter2);
+                binding.courseList.setOnScrollListener(new AbsListView.OnScrollListener() {
+                    @Override
+                    public void onScrollStateChanged(AbsListView view, int scrollState) {
+                    }
+                    @Override
+                    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                    }
+                });
+
+                //enable selecting course from ListView
+                binding.courseList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        if (binding.searchCourse.getVisibility() == View.VISIBLE) {
+                            String added = adapterView.getItemAtPosition(i).toString().trim();
+                            if (!completedCourses.contains(added)) {
+                                completedCourses.add(added);
+                                Collections.sort(completedCourses);
+                                Toast.makeText(completedCourses.this, "Adding succeed!", Toast.LENGTH_SHORT).show();
+                                FirebaseDatabase.getInstance().getReference().child("Users")
+                                        .child("Students").child(FirebaseAuth.getInstance()
+                                                .getCurrentUser().getUid()).child("completedCoursesCode").setValue(completedCourses);
+                            }else{
+                                Toast.makeText(completedCourses.this, "Adding failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+
+                //enable search course
+                binding.searchCourse.setIconifiedByDefault(false);
+                binding.searchCourse.setSubmitButtonEnabled(true);
+                binding.searchCourse.onActionViewExpanded();
+                binding.searchCourse.setQueryHint("Enter course code");
+                binding.searchCourse.setBackgroundColor(0x22ff00ff);
+
+                binding.searchCourse.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+                    private String TAG = getClass().getSimpleName();
+                    @Override
+                    public boolean onQueryTextChange(String queryText) {
+                        arrayAdapter2.getFilter().filter(queryText);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        Log.d(TAG, "onQueryTextSubmit = " + query);
+                        arrayAdapter2.getFilter().filter(query);
+                        if (code.contains(query.trim()) && (!completedCourses.contains(query))){
+                            completedCourses.add(query);
+                            Collections.sort(completedCourses);
+                            Toast.makeText(completedCourses.this, "Adding succeed!", Toast.LENGTH_SHORT).show();
+                            FirebaseDatabase.getInstance().getReference().child("Users")
+                                    .child("Students").child(FirebaseAuth.getInstance()
+                                            .getCurrentUser().getUid()).child("completedCoursesCode").setValue(completedCourses);
+                            return true;
+                        }else{
+                            Toast.makeText(completedCourses.this, "Adding failed!", Toast.LENGTH_SHORT).show();
+                            return false;
+                        }
+                    }
+                });
             }
         });
     }
